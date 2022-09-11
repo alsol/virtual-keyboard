300becc386700074751c2950af9078f45865c985
Alexander Solovyov <xerox.vrn@gmail.com>
2022-09-11 21:17:39 +0300

    Add CLI arguments

--- src/audio.rs
+++ src/audio.rs
@@ -3,17 +3,31 @@ extern crate cpal;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use std::sync::mpsc::Sender;
 
-pub fn init(sender: Sender<f32>) {
+pub struct AudioConfig {
+    pub input_device: String,
+    pub input_channel: usize
+}
+
+pub fn init(sender: Sender<f32>, config: &AudioConfig) {
     let host = cpal::default_host();
+    
+    let input_device = if config.input_device == "default" {
+        host.default_input_device()
+    } else {
+        host.input_devices().unwrap()
+            .find(|d| d.name().map(|name| name == config.input_device).unwrap_or(false))
+    }
+    .expect("Failed to find input device");
 
-    let input_device = host.default_input_device().expect("No input device found");
+    println!("🔊 Input device: {}", input_device.name().unwrap());
 
-    println!("Input device: {}", input_device.name().unwrap());
-
-    let config = input_device
-        .default_input_config()
-        .expect("Failed to get default input config")
+    let input_config = input_device.supported_input_configs().unwrap().enumerate()
+        .find(|(index, _)| index == &(config.input_channel - 1))
+        .map(|(_, c)| c.with_max_sample_rate())
+        .expect("Failed to get input config")
         .into();
+
+    println!("🔨 Selected config: {:?}", input_config);
 
     let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
         for &sample in data {
@@ -22,12 +36,12 @@ pub fn init(sender: Sender<f32>) {
     };
     
     let input_stream = input_device.build_input_stream(
-        &config,
+        &input_config,
         input_data_fn,
         err_fn
     ).expect("Failed to build input stream");
 
-    input_stream.play().expect("Failed to start playing");
+    input_stream.play().expect("Failed to open input stream");
 
     loop {
 
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,58 @@ use ringbuf::RingBuffer;
 use std::sync::mpsc;
 use std::thread;
 use midi::Note;
+use clap::arg;
 
 mod audio;
 mod fft;
 mod midi;
 
 struct Props {
+    audio_config: audio::AudioConfig,
     buffer_size: usize,
     threshold: f32,
+    debug: bool
 }
 
 impl Props {
-    fn new() -> Self {
-        Self {
-            buffer_size: 4_096,
-            threshold: 250.0,
+    fn from_args() -> Self {
+        let app = clap::Command::new("forward")
+            .arg(arg!([IN] "The input device to use [default: default]"))
+            .arg(arg!(-c --channel [CHANNEL] "The input channel for input device [default: 1]"))
+            .arg(arg!(-t --threshold [THRESHOLD] "Specify the threshold for input signal [default: 250]"))
+            .arg(arg!(-d --debug "Produce execution debug output [default: false]"));
+
+        let matches = app.get_matches();
+
+        let input_device = matches.value_of("IN").unwrap_or("Scarlett Solo USB").to_string();
+        let input_channel: usize = matches
+            .value_of("channel")
+            .unwrap_or("2")
+            .parse()
+            .unwrap();
+        let threshold: f32 = matches
+            .value_of("threshold")
+            .unwrap_or("250")
+            .parse()
+            .unwrap();
+
+        let debug: bool = matches.is_present("debug");
+        let buffer_size = 8_192;
+
+        Self { 
+            audio_config: audio::AudioConfig {
+                input_device,
+                input_channel
+            },
+             buffer_size,
+             threshold, 
+             debug 
         }
     }
 }
 
 fn main() {
-    let props = Props::new();
+    let props = Props::from_args();
 
     let (sample_tx, sample_rx) = mpsc::channel();
     let (note_tx, note_rx) = mpsc::channel();
@@ -32,7 +63,7 @@ fn main() {
     let (mut prod, mut cons) = ring_buffer.split();
 
     thread::spawn(move || {
-        audio::init(sample_tx);
+        audio::init(sample_tx, &props.audio_config);
     });
 
     thread::spawn(move || loop {
@@ -53,9 +84,14 @@ fn main() {
                 let maxima = fft::find_maxima(&fft, props.threshold);
 
                 let note = Note::new(
-                    fft::recalculate_to_freq(maxima.1, props.buffer_size, 44_100.0),
+                    fft::recalculate_to_freq(maxima.1, props.buffer_size, 48_000.0),
                     maxima.0,
                 );
+
+                if props.debug && !&note.is_dead() {
+                    println!("{:?}", &note)
+                }
+
                 note_tx.send(note).unwrap();
             }
         }
